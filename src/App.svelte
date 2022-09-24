957ba7348899f2565f458d1a2b6e55d74ef6d0f6
AgricolaJKB <64554282+AgricolaJKB@users.noreply.github.com>
2022-09-24 23:06:27 +0200

    add morse and sound scripts

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,28 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import { encodeString } from "./scripts/morse";
+  import { sonify } from "./scripts/audio";
+
+  const text = "Tes";
+  const encodedText = encodeString("Testtest");
+
+  const handleClick = () => {
+    const morseCharacters = [...encodedText];
+    console.log(morseCharacters);
+    morseCharacters.forEach((c, i) => {
+      setTimeout(() => {
+        sonify(c);
+      }, i * 200);
+    });
+  };
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
+  <p>{text}</p>
 
   <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    {encodedText}
   </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <button on:click={handleClick}>Ton</button>
 </main>
 
 <style>
---
+++ src/scripts/morse.js
@@ -0,0 +1,73 @@
+const MAPPING = {
+  // letters
+  a: ".-",
+  b: "-...",
+  c: "-.-.",
+  d: "-..",
+  e: ".",
+  f: "..-.",
+  g: "--.",
+  h: "....",
+  i: "..",
+  j: ".---",
+  k: "-.-",
+  l: ".-..",
+  m: "--",
+  n: "-.",
+  o: "---",
+  p: ".--.",
+  q: "--.-",
+  r: ".-.",
+  s: "...",
+  t: "-",
+  u: "..-",
+  v: "...-",
+  w: ".--",
+  x: "-..-",
+  y: "-.--",
+  z: "--..",
+  // numbers
+  1: ".----",
+  2: "..---",
+  3: "...--",
+  4: "....-",
+  5: ".....",
+  6: "-....",
+  7: "--...",
+  8: "---..",
+  9: "----.",
+  0: "-----",
+  // punctuation
+  ".": ".-.-.-",
+  ",": "--..--",
+  "?": "..--..",
+  "'": ".----.",
+  "!": "-.-.--",
+  "/": "-..-.",
+  "(": "-.--.",
+  ")": "-.--.-",
+  "&": ".-...",
+  ":": "---...",
+  ";": "-.-.-.",
+  "=": "-...-",
+  "+": ".-.-.",
+  "-": "-....-",
+  _: "..--.-",
+  '"': ".-..-.",
+  $: "...-..-",
+  "@": ".--.-.",
+  " ": "/",
+};
+
+export const encodeCharacter = (character) => {
+  if (MAPPING[character]) {
+    return MAPPING[character];
+  } else {
+    throw `morse code for ${character} is not defined`;
+  }
+};
+
+export const encodeString = (string) => {
+  const lowerCaseString = string.toLowerCase();
+  return [...lowerCaseString].map((c) => encodeCharacter(c)).join(" ");
+};
